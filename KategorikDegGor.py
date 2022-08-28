c0961e795ffff29044c5ea2686627fc91b91a5de
Hasan Fatih Yıldırgan <fatihyildirgan@hotmail.com>
2022-08-28 21:33:25 +0300

    matplotlib,seaborn

--- KategorikDegGor.py
+++ KategorikDegGor.py
@@ -4,6 +4,11 @@
 # Sayisal degisken: histogram, boxplot
 
 # Kategorik Degisken Gorsellestirme
+import pandas as pd
+import seaborn as sns
+pd.set_option('display.max_columns', None)
+df = sns.load_dataset("titanic")
+df.head()
 
 
 df["sex"].value_counts().plot(kind='bar')
